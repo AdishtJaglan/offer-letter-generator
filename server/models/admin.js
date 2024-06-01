8f8b62b96dfa3ddf5b9465326142ecd4535b9a43
AdishtJaglan <adisht7524@gmail.com>
2024-06-01 20:20:17 +0530

    create registration route

--- server/models/admin.js
+++ server/models/admin.js
@@ -1,5 +1,5 @@
-import mongoose, { mongo } from "mongoose";
-const Schema = { mongoose };
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 
 const AdminSchema = new Schema({
   name: {
