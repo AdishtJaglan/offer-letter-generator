935e76f8513a1edc688ce5839dad3a7b13973448
AdishtJaglan <adisht7524@gmail.com>
2024-06-01 17:02:14 +0530

    create admin model:

---
+++ server/models/admin.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+const Schema = { mongoose };
+
+const AdminSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const Admin = mongoose.model("Admin", AdminSchema);
+export default Admin;
