ca89c2279949738b71c5f9d24379b636534a4e03
AdishtJaglan <adisht7524@gmail.com>
2024-06-24 13:39:25 +0530

    add middleware to limit admins to 5

--- server/models/admin.js
+++ server/models/admin.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import { hashPasswordMiddleware } from "../middlewares/hashPasswordMiddleware.js";
 const { Schema } = mongoose;
+const MAX_ADMINS = 5;
 
 const AdminSchema = new Schema({
   name: {
@@ -32,6 +33,16 @@ AdminSchema.pre("findOneAndUpdate", async function (next) {
     } catch (error) {
       return next(error);
     }
+  }
+  next();
+});
+
+AdminSchema.pre("save", async function (next) {
+  const adminCount = await Admin.countDocuments();
+  if (adminCount >= MAX_ADMINS) {
+    const error = new Error(`Admin limit of ${MAX_ADMINS} reached.`);
+    error.status = 403;
+    return next(error);
   }
   next();
 });
